ddd21ddbe598069327a8e33c59687a704b5838d7
Arjo Chakravarty <arjo129@gmail.com>
2016-08-31 09:49:05 +0800

    packaged for pypi and pip

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="websock",
+	version = "0.0.1",
+	description = "Damn simple websocket server",
+	url = "https://arjo129.github.io/",
+	author = "Arjo Chakravarty",
+	author_email="arjo129@gmail.com",
+	license ="MIT",
+	packages = ["websock"],
+	zip_safe = False)
