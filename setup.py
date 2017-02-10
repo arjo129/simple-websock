eb96c7e17dad62629eb62502dad0547abe1d72f5
Arjo Chakravarty <arjo129@gmail.com>
2017-02-10 16:57:10 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name="websock",
 	version = "0.0.1",
 	description = "Damn simple websocket server",
-	url = "https://arjo129.github.io/",
+	url = "https://arjo129.wordpress.com/",
 	author = "Arjo Chakravarty",
 	author_email="arjo129@gmail.com",
 	license ="MIT",
